eae8dfcb94495e8a5861d8954990302e433c62fe
Marcelo Lopez <marcelolopez136@gmail.com>
2019-11-20 02:29:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
+['I LOVE YOU GRANDMA']
+
+end
+
 # unless you shout it (type in all capitals).
 
 # If you shout, she can hear you (or at least she thinks so) 
@@ -10,4 +12,4 @@
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# 'I LOVE YOU TOO PUMPKIN!' 
